47bdd476de8c81a5586e35ebc3828f3328ec80de
andrew-lee2 <leeandrew2@gmail.com>
2017-02-27 15:37:27 -0600

    finished adding in examples, still need to write tests and document

--- simple_paths_algorithm/examples/examples.py
+++ simple_paths_algorithm/examples/examples.py
@@ -1,14 +1,23 @@
 import pandas as pd
+import pkg_resources
 
-def load_csv(file_path):
+def load_csv(csv_name):
     '''
     placeholder
     '''
-    return pd.read_csv(file_path, header=None)
+    csv_path = pkg_resources.resource_filename(__name__, 'data/' + csv_name)
+    csv_df = pd.read_csv(csv_path, header=None)
 
-# need to return 2 dataframes here
-def example_2():
+    return csv_df
+
+def example_graph_2():
     '''
     placeholder
     '''
     return load_csv('ex_2_nodes.csv'), load_csv('ex_2_edges.csv')
+
+def ncaa_2016_data():
+    '''
+    placeholder
+    '''
+    return load_csv('ncaa_2016_teams.csv'), load_csv('ncaa_2016_games.csv')
