48a68c7cf6186fa479f945b612ae2d26cfaec7bd
andrew-lee2 <leeandrew2@gmail.com>
2017-03-30 09:05:03 -0500

    package actually working on pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+DESCRIPTION = (
+    '''
+    implementation of simple paths algorithm
+    '''
+)
 
 setup(
     name='simple_paths_algorithm',
-    version='0.0.2',
-    description='implementation of simple paths algorithm',
+    version='0.0.6',
+    description=DESCRIPTION,
     url='https://github.com/andrew-lee2/simple_paths_algorithm',
     author='Andrew Lee, Douglas Shier',
     author_email='leeandrew2@gmail.com',
     license='MIT',
-    packages=['simple_paths_algorithm'],
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     install_requires=[
